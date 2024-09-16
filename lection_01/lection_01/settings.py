f802dcde428ea8cfa19e28a46f6451a384263399
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-17 00:04:05 +0300

    Add postgre as DB

--- lection_01/lection_01/settings.py
+++ lection_01/lection_01/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'lection_01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lection_01',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
