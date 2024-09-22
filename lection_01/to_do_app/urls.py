c1dbc02a69ec80a310af58efa50af004b4ea0550
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-23 00:07:10 +0300

    Add additional urls

--- lection_01/to_do_app/urls.py
+++ lection_01/to_do_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import handler404
 from . import views
+
+handler404 = 'to_do_app.views.custom_404'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('task/<task_name>/', views.task_by_name)
 ]
