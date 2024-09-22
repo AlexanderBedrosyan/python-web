7fad4ec4154d6aac07b9fb322af10d2f66f95756
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-23 00:07:28 +0300

    Add 2 more views

--- lection_01/to_do_app/views.py
+++ lection_01/to_do_app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import ToDoList
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'base.html')
+    all_tasks = None
+    if request.GET.get('mybtn'):
+        all_tasks = ToDoList.objects.all()
+    tasks_content = {
+        "tasks": all_tasks
+    }
+
+    return render(request, 'base.html', tasks_content)
+
+
+def task_by_name(request, task_name):
+
+
+    return HttpResponse(f"Task name is: {task_name}")
+
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
