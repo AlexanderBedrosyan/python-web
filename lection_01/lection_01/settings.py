c85589dcf470c22be2af2ef5a83ab257f8cacd5a
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-24 08:53:16 +0300

    Add update settings.py

--- lection_01/lection_01/settings.py
+++ lection_01/lection_01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-shy)qc9mt@!97(ke$d62j#u^xv%#crmb_fseag48f#&0g#%m4r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] #'localhost', '127.0.0.1'
 
 
 # Application definition
