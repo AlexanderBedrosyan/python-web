119b0b105a4496816835c80cb6ef858294befe74
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-29 00:14:47 +0300

    Update settings

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -30,6 +30,13 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MY_APPS = [
+    'djangoProject.pets',
+    'djangoProject.common',
+    'djangoProject.accounts',
+    'djangoProject.photos',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
