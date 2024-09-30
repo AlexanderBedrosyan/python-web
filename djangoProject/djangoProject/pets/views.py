738d49b5730ea5d3b156f3669aac72c80fd2829c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-09-30 12:17:29 +0300

    Add all pet views

--- djangoProject/djangoProject/pets/views.py
+++ djangoProject/djangoProject/pets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_pets(request):
+    return render(request, template_name='pets/pet-add-page.html')
+
+def pets_details(request):
+    return render(request, template_name='pets/pet-details-page.html')
+
+def edit_page(request):
+    return render(request, template_name='pets/pet-edit-page.html')
+
+def delete_page(request):
+    return render(request, template_name='pets/pet-delete-page.html')
