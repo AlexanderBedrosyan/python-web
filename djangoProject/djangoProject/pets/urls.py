0fda20e7dca473bd1a9e12f3cf17d3c223c1c4a0
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-01 21:22:15 +0300

    Add urls

--- djangoProject/djangoProject/pets/urls.py
+++ djangoProject/djangoProject/pets/urls.py
@@ -1,1 +1,13 @@
- 
+from django.urls import path, include
+
+from djangoProject.pets import views
+
+urlpatterns = [
+    path('add/', views.add_pets, name='add-pets'),
+    path('<str:username>/pet/', include([
+        path('<slug:pet_slug>', views.pets_details, name='pets-details'),
+        path('<slug:pet_slug>/edit', views.edit_page, name='edit-details'),
+        path('<slug:pet_slug>/delete', views.delete_page, name='delete-details'),
+    ]))
+]
+
