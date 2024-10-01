dbfddc3791645747ea5b19effb3ab7105ce507ee
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-01 21:21:49 +0300

    Add admin

--- djangoProject/djangoProject/pets/admin.py
+++ djangoProject/djangoProject/pets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from djangoProject.pets.models import Pet
+
 # Register your models here.
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
