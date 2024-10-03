b955e06f428ed54554e762bfb74906a87f94e681
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-04 00:03:28 +0300

    Add update admin

---
+++ djangoProject/djangoProject/photos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from djangoProject.photos.models import Photo
+
+# Register your models here.
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_of_publication', 'description', 'get_tagged_pets')
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join([pet.name for pet in obj.tagged_pets.all()])
