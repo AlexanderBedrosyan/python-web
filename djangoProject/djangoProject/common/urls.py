57a67e8bf00c920fe0dcdfc32d88bdd2e41db52c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-03 16:33:42 +0300

    Update file urls

--- djangoProject/djangoProject/common/urls.py
+++ djangoProject/djangoProject/common/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
-from djangoProject.accounts import views
+from djangoProject.common import views
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('', views.home, name='home')
 ]
 
