058cede556297aff26b378b7359a36ee9f4e350b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-07 09:12:20 +0300

    Update urls.py

--- lection_01/to_do_app/urls.py
+++ lection_01/to_do_app/urls.py
@@ -6,5 +6,12 @@ handler404 = 'to_do_app.views.custom_404'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('task/<task_name>/', views.task_by_name)
+    path('task/<task_name>/', views.task_by_name),
+    path('forms', views.forms, name='forms'),
+    path('diff_form_fields', views.diff_form_fields, name="diff_form"),
+    path('inheritance_html', views.inheritance_html, name="inherit_html"),
+    path('test_inherit', views.test_inherit, name="test_inherit"),
+    path('custom_tag', views.custom_tag_example, name="custom_tag"),
+    path('forms_advanced', views.forms_advanced, name="forms_advanced"),
+    path('modelform_factory', views.modelform_factory_views, name="modelform_factory"),
 ]
