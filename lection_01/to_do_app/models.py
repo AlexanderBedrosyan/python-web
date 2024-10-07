8188a149b8140b5436b2b296f2cb46fcb316bf6e
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-07 09:12:56 +0300

    Update models.py

--- lection_01/to_do_app/models.py
+++ lection_01/to_do_app/models.py
@@ -10,3 +10,23 @@ class ToDoList(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Book(models.Model):
+
+    title = models.CharField(
+        max_length=20
+    )
+    author = models.CharField(
+        max_length=50
+    )
+
+from django.db import models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
