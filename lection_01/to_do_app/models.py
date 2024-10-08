b506b36da44ddb203edbb4f1546d0448aaafce10
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-08 09:08:16 +0300

    Update models.py

--- lection_01/to_do_app/models.py
+++ lection_01/to_do_app/models.py
@@ -26,7 +26,10 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+    birth_date = models.DateField(
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
