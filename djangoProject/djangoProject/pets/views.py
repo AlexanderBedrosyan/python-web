771f3b16aa940b7a2fd4b086a5f92e5a0bad729d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-10 15:16:50 +0300

    Update views pets

--- djangoProject/djangoProject/pets/views.py
+++ djangoProject/djangoProject/pets/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from .models import Pet
 
 # Create your views here.
 
 def add_pets(request):
     return render(request, template_name='pets/pet-add-page.html')
 
-def pets_details(request):
-    return render(request, template_name='pets/pet-details-page.html')
+def pets_details(request, username: str, pet_slug: str):
+    pet = Pet.objects.get(slug=pet_slug)
+
+    context = {
+        'pet': pet
+    }
+    return render(request, 'pets/pet-details-page.html', context)
+
 
 def edit_page(request):
     return render(request, template_name='pets/pet-edit-page.html')
