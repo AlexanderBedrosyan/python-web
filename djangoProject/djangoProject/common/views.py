7d36bddb604e47317316f445d44a75efea62dc17
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-10 15:16:36 +0300

    Update views common

--- djangoProject/djangoProject/common/views.py
+++ djangoProject/djangoProject/common/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 
+from djangoProject.common.models import Like
+from djangoProject.photos.models import Photo
+
+
 # Create your views here.
 
 def home(request):
-    return render(request, template_name='common/home-page.html')
+    all_photos = Photo.objects.all()
+    context = {
+        'all_photos': all_photos
+    }
+    return render(request, template_name='common/home-page.html', context=context)
+
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    liked_object = Like.objects.filter(to_photo_id=photo_id).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo=photo)
+        like.save()
+    return render(request.META.get('HTTP_REFERER') + f'#{photo_id}', template_name='common/home-page.html')
