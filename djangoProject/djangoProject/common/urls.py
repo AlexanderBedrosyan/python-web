d1c30e352bbcbd75cfcb80151c4b38eb46901d4f
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-12 08:49:53 +0300

    Update urls

--- djangoProject/djangoProject/common/urls.py
+++ djangoProject/djangoProject/common/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from djangoProject.common import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like'),
+    path('share/<int:photo_id>/', views.copy_link_to_clipboard, name='share'),
 ]
 
