af158647ce3bb6d5b77ee3fc334b9671eff901ab
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-10-14 09:02:17 +0300

    Update views.py

--- djangoProject/djangoProject/pets/views.py
+++ djangoProject/djangoProject/pets/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import PetEditForm
 from .models import Pet
+from ..common.forms import CommentForm
+
 
 # Create your views here.
 
@@ -8,15 +12,33 @@ def add_pets(request):
 
 def pets_details(request, username: str, pet_slug: str):
     pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+    comment_form = CommentForm()
 
     context = {
-        'pet': pet
+        'pet': pet,
+        'all_photos': all_photos,
+        'comment_form': comment_form,
     }
-    return render(request, 'pets/pet-details-page.html', context)
+
+    return render(request, 'pets/pet-details-page.html', context=context)
 
 
-def edit_page(request):
-    return render(request, template_name='pets/pet-edit-page.html')
+def edit_page(request, username: str, pet_slug: str):
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetEditForm(request.POST or None, instance=pet)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+
+    context = {
+        "form": form,
+        "pet": pet,
+    }
+
+    return render(request, 'pets/pet-edit-page.html', context)
 
 def delete_page(request):
     return render(request, template_name='pets/pet-delete-page.html')
