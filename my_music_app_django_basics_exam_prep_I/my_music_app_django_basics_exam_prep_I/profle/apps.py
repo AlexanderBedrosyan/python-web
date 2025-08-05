5943c31f86c51efdb11d0bbc62d956dea3ea65c8
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-08-05 17:07:29 +0300

    Add apps

---
+++ my_music_app_django_basics_exam_prep_I/my_music_app_django_basics_exam_prep_I/profle/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_music_app_django_basics_exam_prep_I.profle'
